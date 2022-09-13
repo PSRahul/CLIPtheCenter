d8e3b08cb434ea82c720b0fe7c2e968e84585715
PSRahul <rahulpsr95@gmail.com>
2022-09-13 23:56:33 +0200

    Working BBox,Center and Embedding Training

--- good_models.py
+++ good_models.py
@@ -1,2 +1,4 @@
 1. "/home/psrahul/MasterThesis/repo/Phase3/CLIPandDetect/trained_models/13092022_2013/epoch-999-loss-0.0170772"
 Working BBox and Center Training
+2. "/home/psrahul/MasterThesis/repo/Phase3/CLIPandDetect/trained_models/13092022_2340/"
+Working BBox,Center and Embedding Training
